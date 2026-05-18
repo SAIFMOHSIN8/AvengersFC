669fd3330a130e7f05c0637c59a48851445dd5e5
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-18 09:25:11 +0400

    86exmp18k: initialize Spring Boot project

---
+++ src/main/java/com/avengersfc/AvengersFcApplication.java
@@ -0,0 +1,13 @@
+package com.avengersfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersFcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengersFcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avengersfc/AvengersFcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avengersfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersFcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
