7a62f1c03593c18efef4922dc0bb18e6898b1020
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-05-19 08:40:37 +0400

    change the id variable type from int to String to mach the UUID format

--- src/main/java/com/avengersfc/controller/FeedbackController.java
+++ src/main/java/com/avengersfc/controller/FeedbackController.java
@@ -8,20 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/feedback")
 public class FeedbackController {
 
     private List<Feedback> feedbackList = new ArrayList<>();
-    private int currentId = 1;
+    private String currentId;
 
     @PostMapping
     public Feedback createFeedback(@RequestBody Feedback feedback) {
 
         feedback.setId(currentId);
 
-        currentId++;
+        currentId= UUID.randomUUID().toString();
 
         feedbackList.add(feedback);
 
@@ -35,7 +37,7 @@ public class FeedbackController {
     }
 
     @GetMapping(path = "/{id}")
-    public Feedback getFeedbackByID(@PathVariable int id) {
+    public Feedback getFeedbackByID(@PathVariable String id) {
         for (Feedback feedback : feedbackList) {
             if (feedback.getId() == id) {
                 return feedback;
@@ -46,7 +48,7 @@ public class FeedbackController {
 
     // Delete end point
     @DeleteMapping(path ="{id}")
-    public void deleteFeedbackByID(@PathVariable int id) {
-        feedbackList.removeIf(feedback -> feedback.getId() == id);
+    public void deleteFeedbackByID(@PathVariable String id) {
+        feedbackList.removeIf(feedback -> Objects.equals(feedback.getId(), id));
     }
 }
